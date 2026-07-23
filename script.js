5f7296f63f12cff7f72dcb2a14e1b6bb6cee0e99
vilinyla <104132564+vilinyla@users.noreply.github.com>
2026-07-23 18:58:41 +0300

    dark mode

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ links.forEach(link => {
     link.addEventListener("click", function(event) {
         if (this.getAttribute("href") === "#") {
             event.preventDefault();
-            alert(`${this.textContent} clicked`);
+            alert(`${this.textContent.trim()} clicked`);
         }
     });
 });
+
+
+const toggle = document.getElementById("themeToggle");
+
+if (toggle) {
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        toggle.textContent = 
+            document.body.classList.contains("dark-mode")
+            ? "☀️"
+            : "🌙";
+    });
+}
